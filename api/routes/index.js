27adf343c852a76bf85f4320fcc3e8373aaf8173
Andrey Ivanov <andrey-tm203@mail.ru>
2018-01-25 11:52:30 +0300

    finishing auth

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 const ctrlBlog = require('../controllers/blog');
 const ctrlSkills = require('../controllers/skills');
 const ctrlWorks = require('../controllers/works');
+const ctrlUser = require('../controllers/user');
 
 router.get('/blog', ctrlBlog.getArticles);
 router.post('/blog', ctrlBlog.createArticle);
@@ -13,5 +14,7 @@ router.put('/skills', ctrlSkills.editSkills);
 
 router.get('/works', ctrlWorks.getWorks);
 router.post('/works', ctrlWorks.createWork);
+
+router.post('/user', ctrlUser.isAuth);
 
 module.exports = router;
